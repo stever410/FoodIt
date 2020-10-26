cc13e73bd290cd919867f0ff9053735985ba55b6
stever410 <ngotanduc410@gmail.com>
2020-10-26 16:37:02 +0700

    Revert to 60cd5c0738a6f181b99364d4c5d2b77b385031dc

--- FoodIt/FoodIt.dtos/Recipe.cs
+++ FoodIt/FoodIt.dtos/Recipe.cs
@@ -4,50 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodIt.FoodIt.dtos
+namespace FoodIt.Foodit.dtos
 {
     public class Recipe
     {
-        public const string DEFAULT_STATUS = "active";
-        private int id;
+        private string recipeId;
         private string email;
         private string title;
         private string description;
         private string status;
         private DateTime date;
         private string image;
-        private List<RecipeIngredient> recipeIngredients;
-        private List<RecipeStep> recipeSteps;
+        private string category;
 
-        public Recipe(string email, string title, string description, string status, DateTime date, string image)
-        {
-            this.email = email;
-            this.title = title;
-            this.description = description;
-            this.status = status;
-            this.date = date;
-            this.image = image;
-        }
-
-        public Recipe(int id, string email, string title, string description, string status, DateTime date, string image)
-        {
-            this.Id = id;
-            this.Email = email;
-            this.Title = title;
-            this.Description = description;
-            this.Status = status;
-            this.Date = date;
-            this.Image = image;
-        }
-
-        public int Id { get => id; set => id = value; }
+        public string RecipeId { get => recipeId; set => recipeId = value; }
         public string Email { get => email; set => email = value; }
         public string Title { get => title; set => title = value; }
         public string Description { get => description; set => description = value; }
         public string Status { get => status; set => status = value; }
         public DateTime Date { get => date; set => date = value; }
         public string Image { get => image; set => image = value; }
-        public List<RecipeIngredient> RecipeIngredients { get => recipeIngredients; set => recipeIngredients = value; }
-        public List<RecipeStep> RecipeSteps { get => recipeSteps; set => recipeSteps = value; }
+        public string Category { get => category; set => category = value; }
+
+        public Recipe(string recipeId, string email, string title, 
+            string description, string status, DateTime date, 
+            string image, string category)
+        {
+            RecipeId = recipeId;
+            Email = email;
+            Title = title;
+            Description = description;
+            Status = status;
+            Date = date;
+            Image = image;
+            Category = category;
+        }
+
+        public Recipe(string recipeId, string title, 
+            DateTime date, string image, string category)
+        {
+            RecipeId = recipeId;
+            Title = title;
+            Date = date;
+            Image = image;
+            Category = category;
+        }
+
+        public Recipe()
+        {
+            RecipeId = recipeId;
+            Title = "";
+            Image = "";
+            Category = "";
+        }
     }
 }
